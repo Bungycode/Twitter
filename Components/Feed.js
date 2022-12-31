3f9deff9afffb41aebccb9da9675b21f05742287
Andrew Bungcayao <84283930+Bungycode@users.noreply.github.com>
2022-12-30 22:16:26 -0500

    11 add the post section of the feed component (#12)
    * Merge development branch with Input section of the feed component into main (#10)
    
    * Install @heroicons/reactv1 package, configure nextConfig.js with the url for the twitter icon, create Sidebar component and SidebarMenuItem component, upload profile pic image, apply Tailwind CSS styling in global.css file for hover effect, and add Sidebar component to home page.
    
    * Replace profile picture with another picture, add styling to the different sections of the Sidebar Component. and add styling to the main element of the home page.
    
    * Create Feed component and the header section. (#6)
    
    * Merge Sidebar component from development branch into main branch (#5)
    
    * Install @heroicons/reactv1 package, configure nextConfig.js with the url for the twitter icon, create Sidebar component and SidebarMenuItem component, upload profile pic image, apply Tailwind CSS styling in global.css file for hover effect, and add Sidebar component to home page.
    
    * Replace profile picture with another picture, add styling to the different sections of the Sidebar Component. and add styling to the main element of the home page.
    
    * Create Feed Component, create header inside feed Component, use lightning bolt icon for future feature of toggling between top/home view and latest tweets, and initial styling of the feed section.
    
    * twitter - Add the input section of the feed component (#9)
    
    * Merge Feed component/header section from development branch into main branch (#7)
    
    * Install @heroicons/reactv1 package, configure nextConfig.js with the url for the twitter icon, create Sidebar component and SidebarMenuItem component, upload profile pic image, apply Tailwind CSS styling in global.css file for hover effect, and add Sidebar component to home page.
    
    * Replace profile picture with another picture, add styling to the different sections of the Sidebar Component. and add styling to the main element of the home page.
    
    * Create Feed component and the header section. (#6)
    
    * Merge Sidebar component from development branch into main branch (#5)
    
    * Install @heroicons/reactv1 package, configure nextConfig.js with the url for the twitter icon, create Sidebar component and SidebarMenuItem component, upload profile pic image, apply Tailwind CSS styling in global.css file for hover effect, and add Sidebar component to home page.
    
    * Replace profile picture with another picture, add styling to the different sections of the Sidebar Component. and add styling to the main element of the home page.
    
    * Create Feed Component, create header inside feed Component, use lightning bolt icon for future feature of toggling between top/home view and latest tweets, and initial styling of the feed section.
    
    * Create Input component, create mini-profile picture, create textarea, create icons and tweet button, and implement Input component in Feed component. Fix className, Image priority, and import error/warning. Install @tailwindcss/forms and set up plugin in the tailwind.config.js file.
    
    * Add the post section of the feed component

--- Components/Feed.js
+++ Components/Feed.js
@@ -1,17 +1,46 @@
 import { LightningBoltIcon } from "@heroicons/react/outline";
 
 import Input from "./Input";
+import Post from "./Post";
 
 export default function Feed() {
+  const posts = [
+    {
+      id: "1",
+      name: "Andrew Bungcayao",
+      username: "thedev_andrew",
+      userimg: "/profile-picture.jpg",
+      img: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1473&q=80",
+      text: "Love the beach!",
+      timestamp: "2 hours ago",
+    },
+    {
+      id: "2",
+      name: "Andrew Bungcayao",
+      username: "thedev_andrew",
+      userimg: "/profile-picture.jpg",
+      img: "https://images.unsplash.com/photo-1579710758949-3ab36db30f1b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      text: "Let's all drink boxed water!",
+      timestamp: "2 days ago",
+    },
+  ];
+
+  console.log("Feed Componont line 28. posts = " + typeof posts);
+  console.log("Feed Component line 29. posts = ", posts);
+
   return (
     <div className="xl:ml-[370px] border-2 border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow">
       <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
         <h2 className="text-lg sm:text-xl font-bold cursor-pointer">Home</h2>
         <div className="hoverEffect flex items-center justify-center px-0 ml-auto w-9 h-9">
-          <LightningBoltIcon className="h-5"/>
+          <LightningBoltIcon className="h-5" />
         </div>
       </div>
       <Input />
+      {posts.map((post) => {
+        console.log("Feed Component line 41. post = ", post);
+        return <Post key={post.id} post={post} />;
+      })}
     </div>
   );
 }
